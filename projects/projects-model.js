81a0ec3c6608ba46c28faec5ac345cf4056bfe49
chrispetito <christopherpetito@gmail.com>
2019-07-16 14:52:33 -0700

    build post endpoint for projects; comment code

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -5,16 +5,19 @@ module.exports = {
   addProject
 };
 
+//get all projects
 function getProjects() {
   return db("projects");
 }
 
+//add project
 function addProject(project) {
   return db("projects")
     .insert(project)
     .then(([id]) => findById(id));
 }
 
+//find project by project id
 function findById(id) {
   return db("projects")
     .where({ id })
