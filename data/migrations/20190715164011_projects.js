c5a937c5f8c10a98ad44cbf19e200e5f09db6b15
chrispetito <christopherpetito@gmail.com>
2019-07-18 13:03:13 -0700

    add categories table drop

--- data/migrations/20190715164011_projects.js
+++ data/migrations/20190715164011_projects.js
@@ -37,5 +37,7 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists("projects");
+  return knex.schema
+    .dropTableIfExists("projects")
+    .dropTableIfExists("categories");
 };
