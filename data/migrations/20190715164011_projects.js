9069c807065c18dd8baac4c62987624b489e157b
chrispetito <christopherpetito@gmail.com>
2019-07-18 12:39:13 -0700

    add additional columns to projects table

--- data/migrations/20190715164011_projects.js
+++ data/migrations/20190715164011_projects.js
@@ -3,9 +3,20 @@ exports.up = function(knex, Promise) {
   return knex.schema
   .createTable('projects', table => {
       table.increments()
-      table.string('name').notNullable()
-      table.string('url').notNullable()
+      table.string('name').notNullable().unique()
+      table.integer('category_id').notNullable()
+      table.boolean('is_approved').defaultTo(false)
       table.string('description')
+      table.string('hosted_url').notNullable().unique()
+      table.string('frontend_url')
+      table.string('backend_url')
+      table.string('submitted_at').notNullable()
+      table.string('approved_at').defaultTo('')
+      table.string('display_image')
+      table.string('tags')
+      table.boolean('in_development').defaultTo(true)
+      table.boolean('is_live').defaultTo(false)
+      table.boolean('is_featured').defaultTo(false)
   })
 };
 
