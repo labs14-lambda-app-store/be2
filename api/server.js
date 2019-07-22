fbbef763956a9aa12b928f371d9f77d0e9dd7a3f
joshua-disney <joshua.a.disney@gmail.com>
2019-07-22 13:16:28 -0700

    Build Users model and router

--- api/server.js
+++ api/server.js
@@ -7,9 +7,11 @@ server.use(cors());
 
 const projectsRouter = require("../routes/projects/projects-router.js");
 const categoriesRouter = require("../routes/categories/categories-router.js");
+const usersRouter = require("../routes/users/users-router.js");
 
 server.use("/api/projects", projectsRouter);
 server.use("/api/categories", categoriesRouter);
+server.use("/api/users", usersRouter);
 
 const repoUrl = `https://github.com/labs14-lambda-app-store`;
 
