142fff7a14b31ff6becca666b4adf5c8e2dd3cde
joshua-disney <joshua.a.disney@gmail.com>
2019-07-25 10:01:28 -0700

    Fix truncation bug

--- data/seeds/002-projects.js
+++ data/seeds/002-projects.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex("projects")
-    .truncate()
+  return knex
+    .raw("TRUNCATE TABLE projects RESTART IDENTITY CASCADE")
     .then(function() {
       // Inserts seed entries
       return knex("projects").insert([
--- data/seeds/004-tags.js
+++ data/seeds/004-tags.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex("tags")
-    .truncate()
+  return knex
+    .raw("TRUNCATE TABLE tags RESTART IDENTITY CASCADE")
     .then(function() {
       // Inserts seed entries
       return knex("tags").insert([
