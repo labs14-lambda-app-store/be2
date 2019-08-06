b7eca1853779a02cfc2b12701af7ddda21b39a7b
Kayla <6lackmacaroon@gmail.com>
2019-08-06 12:45:24 -0600

    truncate category table with dot raw,
    Co-authored-by: Chris <48688296+chrispetito@users.noreply.github.com>
    Co-authored-by: Josh <44819171+Joshua-Disney@users.noreply.github.com>
    Co-authored-by: James <15709701+Shadowborn@users.noreply.github.com>

--- data/seeds/001-categories.js
+++ data/seeds/001-categories.js
@@ -1,29 +1,31 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex("categories")
-    .truncate()
+  return knex
+    .raw(
+      "TRUNCATE TABLE categories RESTART IDENTITY CASCADE"
+    ) /* This .raw function allows for truncation while being referenced in a foreign key */
     .then(function() {
       // Inserts seed entries
       return knex("categories").insert([
-        { category_name: "Business" },          //1
-        { category_name: "Entertainment" },     //2
-        { category_name: "Education" },         //3
-        { category_name: "Games" },             //4
-        { category_name: "Music" },             //5
-        { category_name: "Medical" },           //6
-        { category_name: "Health & Fitness" },  //7
-        { category_name: "Food & Drink" },      //8
-        { category_name: "Finance" },           //9
-        { category_name: "Books" },             //10
+        { category_name: "Business" }, //1
+        { category_name: "Entertainment" }, //2
+        { category_name: "Education" }, //3
+        { category_name: "Games" }, //4
+        { category_name: "Music" }, //5
+        { category_name: "Medical" }, //6
+        { category_name: "Health & Fitness" }, //7
+        { category_name: "Food & Drink" }, //8
+        { category_name: "Finance" }, //9
+        { category_name: "Books" }, //10
         { category_name: "Social Networking" }, //11
-        { category_name: "Shopping" },          //12
-        { category_name: "Photo & Video" },     //13
-        { category_name: "News" },              //14
-        { category_name: "Navigation" },        //15
-        { category_name: "Sports" },            //16
-        { category_name: "Travel" },            //17
-        { category_name: "Weather" },           //18
-        { category_name: "Miscellaneous" }      //19
+        { category_name: "Shopping" }, //12
+        { category_name: "Photo & Video" }, //13
+        { category_name: "News" }, //14
+        { category_name: "Navigation" }, //15
+        { category_name: "Sports" }, //16
+        { category_name: "Travel" }, //17
+        { category_name: "Weather" }, //18
+        { category_name: "Miscellaneous" } //19
       ]);
     });
 };
