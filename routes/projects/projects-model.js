a9151aa77caee49d9d07166268b3e681d788e2f1
Jeff Lapp <lappjeff20@gmail.com>
2019-08-09 12:52:56 -0400

    Created endpoint to get counts and refactored project fetching

--- routes/projects/projects-model.js
+++ routes/projects/projects-model.js
@@ -7,13 +7,24 @@ module.exports = {
   getProjectTags,
   getProjectCategory,
   getProjectComments,
+  getProjectsCount,
   addProject,
   updateProject,
   deleteProject
 };
 
+//approvedFilter will take in either true or false
+// if true: query DB for all projects where is_approved === true
+// if false: query DB for all projects where is_approved === false
+// need to make sure project length isn't screwed in the process
+// might need to take projectLength state out of Redux or separate into pendingProjectLength and
 //get all projects
-function getProjects(searchParameter) {
+
+//WE NEED THIS
+//total count of all is_approved = true projects with optional search param
+//total count of all is_approved = false projects with optional search param
+
+function getProjects(searchParameter, approvedFilter) {
   if (searchParameter) {
     return db("projects").whereRaw(
       `UPPER(name) LIKE UPPER('%${searchParameter}%') or UPPER(description) LIKE UPPER('%${searchParameter}%')`
@@ -23,19 +34,37 @@ function getProjects(searchParameter) {
   }
 }
 
+//returns the count of all projecs with the searchParameter or approvedFilter
+function getProjectsCount(searchParameter, approvedFilter) {
+  let filter = approvedFilter ? `AND is_approved = ${approvedFilter}` : "";
+  if (approvedFilter && !searchParameter) {
+    return db("projects")
+      .whereRaw(`is_approved = ${approvedFilter}`)
+      .count("id");
+  } else {
+    return db("projects")
+      .whereRaw(
+        `(UPPER(name) LIKE UPPER('%${searchParameter}%') or UPPER(description) LIKE UPPER('%${searchParameter}%')) ${filter}`
+      )
+      .count("id");
+  }
+}
+
 //sets pagination while getting set amount of projects at a time.  Currently 12 projects per page.
-function getProjectsPerPage(page, searchParameter) {
+function getProjectsPerPage(page, searchParameter, approvedFilter) {
+  let filter = approvedFilter ? `AND is_approved = ${approvedFilter}` : "";
   let limit = 12;
   let offset = (page - 1) * limit;
   if (searchParameter) {
     return db("projects")
       .whereRaw(
-        `UPPER(name) LIKE UPPER('%${searchParameter}%') or UPPER(description) LIKE UPPER('%${searchParameter}%')`
+        `(UPPER(name) LIKE UPPER('%${searchParameter}%') or UPPER(description) LIKE UPPER('%${searchParameter}%')) ${filter}`
       )
       .limit(limit)
       .offset(offset);
   } else {
     return db("projects")
+      .whereRaw(`is_approved = ${approvedFilter}`)
       .limit(limit)
       .offset(offset);
   }
--- routes/projects/projects-router.js
+++ routes/projects/projects-router.js
@@ -6,26 +6,71 @@ const Projects = require("./projects-model");
 const environment = process.env.DB_ENV;
 
 //endpoint to get all projects
+// router.get("/", async (req, res) => {
+//   let searchParameter = req.query.search;
+//   let filter = req.query.filter;
+//   let page = parseInt(req.query.page);
+//
+//   try {
+//     const projects = await Projects.getProjects(searchParameter);
+//     const projectsPerPage = await Projects.getProjectsPerPage(
+//       page,
+//       searchParameter
+//     );
+//     for (i = 0; i < projectsPerPage.length; i++) {
+//       const project = projectsPerPage[i];
+//       project.tags = await Projects.getProjectTags(project.id);
+//       project.category = await Projects.getProjectCategory(project.category_id);
+//       project.comments = await Projects.getProjectComments(project.id);
+//     }
+//     res.status(200).json({
+//       projects: projectsPerPage,
+//       projectLength: projects.length,
+//       message: "Did somebody order some projects"
+//     });
+//   } catch (error) {
+//     //only logs the real error if we are not in production
+//     if (environment === "production") {
+//       res.status(500).json({ message: "error getting projects " });
+//     } else {
+//       console.log("Getting projects error:", error);
+//       res
+//         .status(500)
+//         .json({ message: "error getting projects ", error, environment });
+//     }
+//   }
+// });
+
 router.get("/", async (req, res) => {
-  let searchParameter = req.query.search;
-  let page = parseInt(req.query.page) || 1;
+  console.log(req);
+  const searchParameter = req.query.search;
+  const filter = req.query.approved;
+  const page = req.query.page;
 
   try {
-    const projects = await Projects.getProjects(searchParameter);
+    //gets count of all projects requested - necessary for FE pagination
+    const totalFilterProjectsCount = await Projects.getProjectsCount(
+      searchParameter,
+      filter
+    );
+    //gets actual projects requested, with pagination
     const projectsPerPage = await Projects.getProjectsPerPage(
       page,
-      searchParameter
+      searchParameter,
+      filter
     );
+
+    //loops through projects and adds the proper values based off foreign keys to related array on project
     for (i = 0; i < projectsPerPage.length; i++) {
       const project = projectsPerPage[i];
       project.tags = await Projects.getProjectTags(project.id);
       project.category = await Projects.getProjectCategory(project.category_id);
       project.comments = await Projects.getProjectComments(project.id);
     }
+
     res.status(200).json({
-      projects: projectsPerPage,
-      projectLength: projects.length,
-      message: "Did somebody order some projects"
+      length: parseInt(totalFilterProjectsCount[0].count),
+      projects: projectsPerPage
     });
   } catch (error) {
     //only logs the real error if we are not in production
@@ -39,7 +84,6 @@ router.get("/", async (req, res) => {
     }
   }
 });
-
 //endpoint to get project by id
 router.get("/:id", async (req, res) => {
   try {
