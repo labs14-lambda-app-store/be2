cec54b7e9a3da4e5ac1acd85ce2a8cccbaff3867
chrispetito <christopherpetito@gmail.com>
2019-08-15 14:06:06 -0700

    add user spec

--- routes/users/users.spec.js
+++ routes/users/users.spec.js
@@ -3,6 +3,24 @@ const supertest = require("supertest");
 const db = require("../../data/dbConfig");
 const { addUser } = require("./users-model");
 
+const user = {
+  username: "Username",
+  email: "Email",
+  sub_id: 1,
+  first_name: "First",
+  last_name: "Last",
+  pictureURL: "string"
+};
+
+const userTwo = {
+  username: "Username2",
+  email: "Email2",
+  sub_id: 1,
+  first_name: "First",
+  last_name: "Last",
+  pictureURL: "string"
+};
+
 describe("Users", () => {
   beforeEach(async () => {
     await db("users").truncate();
@@ -18,42 +36,42 @@ describe("Users", () => {
         .get("/api/users")
         .expect(200);
     });
+    it("should return a 200 when fetching individual user", async () => {
+      await supertest(server)
+        .post("/api/users")
+        .send(user);
+
+      const res = await supertest(server).get("/api/users/1");
+
+      expect(res.status).toBe(200);
+    });
   });
   describe("POST /", () => {
     it("should return with a 201 status code", async () => {
-      const user = {
-        username: "Username",
-        email: "Email",
-        sub_id: 1,
-        first_name: "First",
-        last_name: "Last",
-        pictureURL: "string"
-      };
       const res = await supertest(server)
         .post("/api/users")
         .send(user);
       expect(res.status).toBe(201);
     });
     it("should insert users", async () => {
-      await addUser({
-        username: "Username1",
-        email: "Email1",
-        sub_id: 1,
-        first_name: "First",
-        last_name: "Last",
-        pictureURL: "string"
-      });
-      await addUser({
-        username: "Username2",
-        email: "Email2",
-        sub_id: 1,
-        first_name: "First",
-        last_name: "Last",
-        pictureURL: "string"
-      });
+      await addUser(user);
+      await addUser(userTwo);
       const users = await db("users");
 
       expect(users).toHaveLength(2);
+    });
+  });
+  describe("DELETE", () => {
+    it("should return a 200 when deleting a user", async () => {
+      await supertest(server)
+        .post("/api/users")
+        .send(user);
+
+      const res = await supertest(server)
+        .delete("/api/users/1")
+        .send(user);
+
+      expect(res.status).toBe(200);
     });
   });
 });
