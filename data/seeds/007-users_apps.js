a369097b69f5c4fc5d07b529e33dbd029cc7be74
Jeff Lapp <lappjeff20@gmail.com>
2019-08-27 18:30:02 -0400

    Added more seeds

--- data/seeds/007-users_apps.js
+++ data/seeds/007-users_apps.js
@@ -11,11 +11,11 @@ exports.seed = function(knex, Promise) {
         { user_id: 4, app_id: 1 },
         { user_id: 5, app_id: 1 },
         { user_id: 6, app_id: 1 },
-        { user_id: 2, app_id: 2 },
         { user_id: 1, app_id: 3 },
         { user_id: 2, app_id: 3 },
         { user_id: 3, app_id: 3 },
-        { user_id: 4, app_id: 3 }
+        { user_id: 4, app_id: 3 },
+        { user_id: 5, app_id: 3 }
       ]);
     });
 };
