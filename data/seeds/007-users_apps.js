10574e86ad87ab102cefd1cdbaeed5e9ba406f29
Jeff Lapp <lappjeff20@gmail.com>
2019-08-27 18:14:40 -0400

    Fixed seed insert names

--- data/seeds/007-users_apps.js
+++ data/seeds/007-users_apps.js
@@ -5,18 +5,18 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex("users_apps").insert([
-        { app_id: 1, tag_id: 1 },
-        { app_id: 1, tag_id: 2 },
-        { app_id: 1, tag_id: 3 },
-        { app_id: 1, tag_id: 4 },
-        { app_id: 1, tag_id: 5 },
-        { app_id: 1, tag_id: 6 },
-        { app_id: 2, tag_id: 1 },
-        { app_id: 2, tag_id: 2 },
-        { app_id: 3, tag_id: 1 },
-        { app_id: 3, tag_id: 3 },
-        { app_id: 3, tag_id: 4 },
-        { app_id: 3, tag_id: 5 }
+        { user_id: 1, app_id: 1 },
+        { user_id: 1, app_id: 2 },
+        { user_id: 1, app_id: 3 },
+        { user_id: 1, app_id: 4 },
+        { user_id: 1, app_id: 5 },
+        { user_id: 1, app_id: 6 },
+        { user_id: 2, app_id: 1 },
+        { user_id: 2, app_id: 2 },
+        { user_id: 3, app_id: 1 },
+        { user_id: 3, app_id: 3 },
+        { user_id: 3, app_id: 4 },
+        { user_id: 3, app_id: 5 }
       ]);
     });
 };
