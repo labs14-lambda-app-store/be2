a4f47e5e3c113fb0b7566f0d5acbb8ef5d8dabc4
Jeff Lapp <lappjeff20@gmail.com>
2019-08-26 20:16:33 -0400

    Added proper catch functionality to routes

--- routes/app-tags/apps-tags-router.js
+++ routes/app-tags/apps-tags-router.js
@@ -23,16 +23,20 @@ router.get("/", async (req, res) => {
 // endpoint to add a new app-tag relation
 router.post("/", async (req, res) => {
   try {
-    req.body.tags.forEach(async tag => {
-      let app_tag = await App_Tags.addAppTag(tag);
-    });
+    const app_tags = await req.body.tags.forEach(tag => {
+      if (!tag.app_id || !tag.tag_id) throw "missing property";
 
+      let app_tag = App_Tags.addAppTag(tag);
+    });
     res.status(201).json({ message: "App_tag successfully created." });
   } catch (error) {
+    console.log(error);
     if (environment === "production") {
       res.status(500).json({ message: "Error creating that app_tag" });
+    } else if (error === "missing property") {
+      res.status(500).json({ message: "Missing app id or tag id" });
     } else {
-      cconsole.log("Create app_tag error : ", error);
+      console.log("Create app_tag error : ", error);
       res.status(500).json({ message: "Error creating that app_tag", error });
     }
   }
--- routes/users-apps/users-apps-router.js
+++ routes/users-apps/users-apps-router.js
@@ -23,11 +23,17 @@ router.get("/", async (req, res) => {
 // Post endpoint to add a UserApp relation
 router.post("/", async (req, res) => {
   try {
-    const userApp = await UsersApps.addUserApp(req.body);
+    const add_users = await req.body.users.forEach(user => {
+      if (!user.user_id || !user.app_id) throw "missing property";
+
+      let user_app = UsersApps.addUserApp(user);
+    });
     res.status(201).json({ message: "UserApp successfully created." });
   } catch (error) {
     if (environment === "production") {
       res.status(500).json({ message: "Error creating that UserApp." });
+    } else if (error === "missing property") {
+      res.status(500).json({ message: "Missing app id or user id" });
     } else {
       console.log("Creating UserApp error ", error);
       res.status(500).json({ message: "Error creating that UserApp.", error });
