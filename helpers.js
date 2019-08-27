dbf26373ca74007f24a128de907fb406ed2f4444
Jeff Lapp <lappjeff20@gmail.com>
2019-08-27 19:04:08 -0400

    Added better error message for dev logs

--- helpers.js
+++ helpers.js
@@ -2,11 +2,17 @@ const environment = process.env.DB_ENV;
 
 //checks if environment is production and returns a safe error message for users
 //else returns message useful for development
+console.log(environment);
 const returnSafeErrorMessage = (res, message, error) => {
   if (environment === "production") {
     res.status(500).json({ message });
   } else {
-    console.log("Get categories error", error);
+    console.log(error);
+    console.log(`
+      ********************
+      ${message}
+      ********************
+      ${error}`);
     res.status(500).json({ message, error });
   }
 };
